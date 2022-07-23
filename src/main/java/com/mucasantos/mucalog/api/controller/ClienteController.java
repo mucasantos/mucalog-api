9651d07b8c64280d4d1c69714758486bb1cc0c4b
Samuel Luis dos Santos <mucasantos@gmail.com>
2022-07-23 15:46:41 -0300

    validation
    Implementando validação

--- src/main/java/com/mucasantos/mucalog/api/controller/ClienteController.java
+++ src/main/java/com/mucasantos/mucalog/api/controller/ClienteController.java
@@ -3,6 +3,8 @@ package com.mucasantos.mucalog.api.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -47,14 +49,14 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionar (@RequestBody Cliente cliente) {
+	public Cliente adicionar (@Valid @RequestBody Cliente cliente) {
 	
 		return clienteRepository.save(cliente);
 	}
 	
 	@PutMapping("/{clienteId}")
 	public ResponseEntity<Cliente> atualizar (@PathVariable Long clienteId, 
-			@RequestBody Cliente cliente){
+		@Valid	@RequestBody Cliente cliente){
 		
 		if(!clienteRepository.existsById(clienteId)) {
 			return ResponseEntity.notFound().build();
